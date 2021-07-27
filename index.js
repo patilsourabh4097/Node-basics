bafbdb6e3b6e6e4b064a8812613f6e42efa61cc5
Sourabh Patil <patilscourses4097@gmail.com>
2021-07-27 16:52:49 +0530

    removed console logs and used const instead of var

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
 
 http.createServer(function(req,res){
     const q = url.parse(req.url, true);
     const filename = q.pathname === "/" ? `./index.html` : `.${q.pathname}.html`
-    console.log(q.pathname);
-
+    
     fs.readFile(filename, function(err, data){
         if(err){
             res.writeHead(404, {'Content-Type':'text/html'});
